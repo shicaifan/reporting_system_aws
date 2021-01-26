5003a3bbb7b98efcd4d111c6da9386ae409d81bc
shicaifan <shicaifan@outlook.com>
2021-01-26 10:36:41 -0800

    update

--- ExcelService/src/main/java/com/antra/evaluation/reporting_system/repo/ExcelRepositoryImpl.java
+++ ExcelService/src/main/java/com/antra/evaluation/reporting_system/repo/ExcelRepositoryImpl.java
@@ -11,28 +11,28 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 @Repository
-public interface ExcelRepositoryImpl extends MongoRepository<ExcelFile, String> {
+public class ExcelRepositoryImpl implements ExcelRepository {
 
-//    Map<String, ExcelFile> excelData = new ConcurrentHashMap<>();
-//
-//    @Override
-//    public Optional<ExcelFile> getFileById(String id) {
-//        return Optional.ofNullable(excelData.get(id));
-//    }
-//
-//    @Override
-//    public ExcelFile saveFile(ExcelFile file) {
-//        return excelData.put(file.getFileId(), file);
-//    }
-//
-//    @Override
-//    public ExcelFile deleteFile(String id) {
-//        return excelData.remove(id);
-//    }
-//
-//    @Override
-//    public List<ExcelFile> getFiles() {
-//        return new ArrayList<>(excelData.values());
-//    }
+    Map<String, ExcelFile> excelData = new ConcurrentHashMap<>();
+
+    @Override
+    public Optional<ExcelFile> getFileById(String id) {
+        return Optional.ofNullable(excelData.get(id));
+    }
+
+    @Override
+    public ExcelFile saveFile(ExcelFile file) {
+        return excelData.put(file.getFileId(), file);
+    }
+
+    @Override
+    public ExcelFile deleteFile(String id) {
+        return excelData.remove(id);
+    }
+
+    @Override
+    public List<ExcelFile> getFiles() {
+        return new ArrayList<>(excelData.values());
+    }
 }
 
